19b89a265b4f8faf275fd206384bd5fe0359807d
jose garcia <thomasrestrepo92@gmail.com>
2025-10-21 16:09:18 -0500

    ejercicios nuevos

--- python/ejercicio1.py
+++ python/ejercicio1.py
@@ -1,6 +1,15 @@
-numeros=float(input("ingrese su valor"))
-cambio=4.000
-cambio=numeros*cambio
-print(f"su valor en dolares es {cambio}")
-
-
+while True:
+    numero=float(input("ingrese su valor"))
+    cambio=4.000
+    try:
+        num= int(numero)
+        if num  >=  1 :
+            print(f"su {num} es valido pra cambiar")
+            cambio=num/cambio
+            print(f"su valor en dolares es {cambio}")
+            break
+        else:
+            print(f"tu {num} no se puede comvertir")
+    except ValueError:
+        print("ingrese un valor real")
+        
